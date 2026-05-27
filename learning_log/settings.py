c0666088759c6eacd0deea6df24257ebce19931c
littleflyingpig <littleflyingpig@zyq.localdomain>
2026-05-27 15:18:56 +0800

    再次修改密钥

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e9bckp4*xb7lf0)&s*h#9c%nw*ky#8uwx-pw*m^8r)h$)z=*zh'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
